9147c4ffdd7481810ab1628205e5b7efe2a4067c
Piotr Semenov <piotr.k.semenov@gmail.com>
2020-11-30 23:19:47 +0300

    implement bash dispatcher for css/js inputs

---
+++ uglify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ $1 == *.css ]]; then
+    uglifycss --expand-vars --max-line-len 500 --output $2 $1;
+elif [[ $1 == *.js ]]; then
+    uglifyjs --compress --mangle --toplevel --output $2 $1;
+fi
