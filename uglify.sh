3d77a315e6b10a04a0091c59542932b6c1d6923a
Piotr Semenov <piotr.k.semenov@gmail.com>
2020-11-30 23:27:57 +0300

    fix, non-POSIX way to remove the largest prefix pattern

--- uglify.sh
+++ uglify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ "$1" = "*.css" ]; then
+if [ "${1##*.}" = "css" ]; then
     uglifycss --expand-vars --max-line-len 500 --output "$2" "$1";
-elif [ "$1" = "*.js" ]; then
+elif [ "${1##*.}" = "js" ]; then
     uglifyjs --compress --mangle --toplevel --output "$2" "$1";
 fi
